1d8de4cd830794b9d26c61dc9a395d9df7e35b2d
AYEESIDATTA SENAPATI <AYEESIDATTA SENAPATI@AICHHIKA-DELL>
2025-08-21 15:43:58 +0530

    Added New Test

--- SeleniumProject_MavenJenkins/src/test/java/pac1/ContactTest.java
+++ SeleniumProject_MavenJenkins/src/test/java/pac1/ContactTest.java
@@ -26,6 +26,10 @@ public class ContactTest {
 	public void modifycontactTest() {
 		System.out.println("execute modifycontactTest");
 		System.out.println("execute modifycontactTest");
-
+	}
+	
+	@Test
+	public void deletecontactTest() {
+		System.out.println("execute deletecontactTest");
 	}
 }
